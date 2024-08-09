b3fcecdb70ee84cfcd172d02b301a24c98247ee0
Zafirdausi <muhammad_ahla_33rpl@student.smktelkom-mlg.sch.id>
2024-08-09 23:08:44 +0700

    Add files via upload

---
+++ Buku.java
@@ -0,0 +1,33 @@
+public class Buku {
+    int id;
+    String judul;
+    String pengarang;
+    int Tahunterbit;
+
+
+
+    public Buku(int id,String judul,String pengarang,int Tahunterbit) {
+        this.id = id;
+        this.judul = judul;
+        this.pengarang = pengarang;
+        this.Tahunterbit = Tahunterbit;
+    }    
+
+        Buku OngkekLiaran = new Buku(1, "OngkekLiaran", "Dafi", 1999);
+        Buku BalapanMasuk = new Buku(2, "BalapanMasuk", "FIRDAUSI", 2000);
+        Buku KeluarDiOut = new Buku(3, "Keluardiluar", "Agus", 2077);
+        Buku CaraagarMudahbesar = new Buku(4, "CaraagarMudah", "Kacongcoki", 1988);
+        Buku CARKECORCONG = new Buku(5, "CARKECOR", "lokqho", 1277);
+
+        //OngkekLiaran
+        System.out.println(OngkekLiar)
+
+}
+
+
+
+    
+      
+
+    
+
